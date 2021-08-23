cb0b1a251f56d9ea64d436f394d7ccb9c90916e0
Matias Gaete <mgaete@boostworld.com>
2021-08-22 21:44:42 -0400

    feat: app js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import StepsProvider from './context/StepContext'
 import './App.css';
+import Wrapper from './components/Wrapper';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<StepsProvider>
+  <div style={{width: '50%', alignContent: 'center', margin:'40px'}}>
+  <Wrapper/>
+  </div>
+</StepsProvider>
+
   );
 }
 
